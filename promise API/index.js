01f0e1f526ad5a0f6ec3cffd2750eba97a7dac37
dontroll17 <dontroll@yandex.ru>
2022-04-13 19:11:51 +0300

    feat: filtrer and delete query

--- promise API/index.js
+++ promise API/index.js
@@ -9,17 +9,26 @@ const connection = createConnection({
 
 (async() => {
     const sql = "INSERT INTO table_name (id, data) VALUES ?";
+    const sqlFil = "UPDATE table_name SET data=? WHERE id=?";
+    const sqlDel = "DELETE FROM table_name WHERE data=?";
+
     const val = [
         [4, 123],
         [5, 321],
         [6, 123]
     ];
-    
+    const upData = [1, 123];
+    const delData = [123];
+
     const query = await connection.query(sql, val);
     const data = await connection.query('SELECT * FROM table_name');
-    
+    const upQuery = await connection.query(sqlFil, upData);
+    const delQuery = await connection.query(sqlDel, delData);
+
     console.log(query[0]);
     console.log(data[0]);
+    console.log(upQuery);
+    console.log(delQuery);
 
     await connection.end();
 })();
